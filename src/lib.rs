7b8d9df724f953f4ec531580a0e17a4f6d69d90f
FL33TW00D <chris@fleetwood.dev>
2024-05-24 15:28:33 +0100

    chore: whitespacing

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,61 @@
-use proc_macro::TokenStream as TokenStream1;
+#![feature(proc_macro_span)]
+
+use std::fmt::Write;
+
+use proc_macro::{Span, TokenStream as TokenStream1};
 use proc_macro2::{Delimiter, Spacing, TokenStream, TokenTree};
-use quote::quote;
+use quote::{quote, quote_spanned};
 
 struct Source {
     modified: String,
+    column: usize,
+    initial_line: Option<usize>,
+    line: usize,
 }
 
 impl Source {
+    fn add_whitespace(
+        &mut self,
+        span: Span,
+        line: usize,
+        column: usize,
+    ) -> Result<(), TokenStream> {
+        let initial_line = *self.initial_line.get_or_insert(line);
+        let line = line.checked_sub(initial_line);
+        let line = line
+            .ok_or_else(|| quote_spanned!(span.into() => compile_error!{"Invalid line number"}))?;
+        if line > self.line {
+            while line > self.line {
+                self.modified.push('\n');
+                self.line += 1;
+            }
+
+            for _ in 0..column {
+                self.modified.push(' ');
+            }
+
+            self.column = column;
+        } else if line == self.line {
+            while column > self.column {
+                self.modified.push(' ');
+                self.column += 1;
+            }
+        }
+
+        Ok(())
+    }
+
     fn add(&mut self, input: TokenStream) -> Result<(), TokenStream> {
         let mut tokens = input.into_iter();
 
         while let Some(token) = tokens.next() {
+            let span = token.span().unwrap();
+            let col = span.column();
+            let line = span.line();
+
+            dbg!(&token);
+            dbg!(col, line);
+            self.add_whitespace(span, span.line(), span.column())?;
             match &token {
                 TokenTree::Group(x) => {
                     let (start, end) = match x.delimiter() {
@@ -20,39 +65,46 @@ impl Source {
                         Delimiter::None => ("", ""),
                     };
                     self.modified.push_str(start);
+                    self.column += start.len();
                     self.add(x.stream())?;
+                    let end_span = token.span().unwrap().end();
+                    self.add_whitespace(
+                        span,
+                        end_span.line(),
+                        end_span.column().saturating_sub(end.len()),
+                    )?;
                     self.modified.push_str(end);
+                    self.column += end.len();
                 }
                 TokenTree::Punct(x) => {
                     // If we find a template parameter, like 'workgroup_size_y, then replace
                     // with {workgroup_size_y}
                     if x.as_char() == '\'' && x.spacing() == Spacing::Joint {
                         if let Some(TokenTree::Ident(name)) = tokens.next() {
+                            let name_str = name.to_string();
                             self.modified.push('{');
-                            self.modified.push_str(&name.to_string());
+                            self.modified.push_str(&name_str);
                             self.modified.push('}');
+                            self.column += name_str.len();
                         } else {
                             return Err(quote! {
                                 compile_error!("Expected a templated parameter after single quote.");
                             });
                         };
                     } else {
-                        self.modified.push_str(&x.to_string());
+                        self.modified.push(x.as_char());
                     }
+                    self.column += 1;
                 }
-                TokenTree::Ident(v) => {
-                    let v_str = v.to_string();
-                    self.modified.push_str(&v.to_string());
-
-                    match v_str.as_str() {
-                        "var" | "let" | "fn" => {
-                            self.modified.push(' ');
-                        }
-                        _ => {}
-                    }
+                TokenTree::Ident(x) => {
+                    write!(&mut self.modified, "{}", x).unwrap();
+                    let end_span = token.span().unwrap().end();
+                    self.column = end_span.column();
                 }
                 _ => {
                     self.modified.push_str(&token.to_string());
+                    let end_span = token.span().unwrap().end();
+                    self.column = end_span.column();
                 }
             }
         }
@@ -64,6 +116,9 @@ impl Source {
 fn inline_wgsl(input: TokenStream) -> Result<TokenStream, TokenStream> {
     let mut source = Source {
         modified: "".to_string(),
+        column: 0,
+        initial_line: None,
+        line: 0,
     };
     let _ = source.add(input);
     let modified = source.modified;
